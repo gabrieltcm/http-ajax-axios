adc4821176efb75e75d30a9d66d44a0162aa9966
Gabriel TCM <gabrieltcm@Gabriels-MacBook-Pro.local>
2021-02-05 17:49:15 +0800

    Initial Commit

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+// import axios from 'axios';
+import { Route, NavLink, Switch, Redirect } from "react-router-dom";
+
+import "./Blog.css";
+import Posts from "./Posts/Posts";
+import NewPost from "./NewPost/NewPost";
+
+class Blog extends Component {
+  state = {
+    auth: false,
+  };
+  s;
+  render() {
+    return (
+      <div className="Blog">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <NavLink
+                  to="/posts/"
+                  exact
+                  activeClassName="my-active"
+                  activeStyle={{
+                    color: "#fa923f",
+                    textDecoration: "underline",
+                  }}
+                >
+                  Posts
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={{
+                    pathname: "/new-post",
+                    hash: "#submit",
+                    search: "?quick-submit=true",
+                  }}
+                >
+                  New Post
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+                <Route path="/" render={() => <h1>Home 2</h1>} /> */}
+        <Switch>
+          {this.state.auth ? (
+            <Route path="/new-post" component={NewPost} />
+          ) : null}
+          <Route path="/posts" component={Posts} />
+          <Redirect from="/" to="/posts" />
+          {/* <Route path="/" component={Posts} /> */}
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default Blog;
